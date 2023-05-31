3a972995f32862afc6036011dd620514da4f76d1
VJT <vjeandot@yahoo.com>
2023-05-31 10:31:55 +0200

    add jitpack configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.20"
+    id("maven-publish")
 }
 
 group = "fr.valentin.lib"
@@ -9,9 +10,24 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-}
-
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group as String
+            artifactId = rootProject.name
+            version = version
+
+            from(components["java"])
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
