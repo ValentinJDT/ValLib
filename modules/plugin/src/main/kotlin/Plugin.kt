047de1606d71e843582b01ee8f33e3fc39c8e130
Valentin JEANDOT <valentin.jeandot@cirly.com>
2025-08-20 16:35:59 +0200

    chore: enhance IPlugin interface with version and description properties

--- modules/plugin/src/main/kotlin/Plugin.kt
+++ modules/plugin/src/main/kotlin/Plugin.kt
@@ -2,7 +2,7 @@ package fr.valentinjdt.lib.plugin
 
 import java.net.URL
 
-abstract class Plugin(override val name: String, override val description: String = ""): IPlugin {
+abstract class Plugin(override val name: String, override val description: String = "", override val version: String): IPlugin {
     override var url: URL by InitOnceProperty()
     override fun onEnable() {}
     override fun onDisable() {}
