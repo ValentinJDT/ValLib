81eb19f37c8865b06357bd0b42b4ce3b05fbbf54
Valentin JEANDOT <valentin.jeandot@cirly.com>
2025-08-24 10:54:30 +0200

    refactor: improve documentation for subCommandsCompletions and SubCommandCompletion data class

--- modules/plugin/src/main/kotlin/Command.kt
+++ modules/plugin/src/main/kotlin/Command.kt
@@ -4,21 +4,21 @@ import java.net.URL
 
 abstract class Command(override val name: String, override val description: String = "", override val version: String): IPlugin {
 
-    /** List of sub-commands for tab completion : works only with Empty Terminal.
-     *
-     *
-     */
+    /** List of sub-commands for tab completion, used for Empty Terminal. */
     abstract val subCommandsCompletions: List<SubCommandCompletion>
 
     override var url: URL by InitOnceProperty()
     /**
      * Execute the command with the given arguments.
      * @param args The arguments passed to the command.
-     * @throws IllegalArgumentException if the command is not valid.
      */
     abstract fun execute(args: List<String>)
     override fun onEnable() {}
     override fun onDisable() {}
 }
 
-data class SubCommandCompletion(val command: String, val subCommands: List<SubCommandCompletion>?)
+data class SubCommandCompletion(
+    /** If null or empty, a input is needed who isn't a command. */
+    val command: String?,
+    val subCommands: List<SubCommandCompletion>?
+)
