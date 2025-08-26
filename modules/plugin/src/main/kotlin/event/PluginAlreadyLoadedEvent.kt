52052e5fcfea0909e2ce4bdc7dc6015a917216bc
Valentin JEANDOT <valentin.jeandot@cirly.com>
2025-08-26 11:53:33 +0200

    feat: introduce plugin lifecycle events with pre-enable, enable, pre-disable, and disable events

--- modules/plugin/src/main/kotlin/event/PluginAlreadyLoadedEvent.kt
+++
@@ -1,5 +0,0 @@
-package fr.valentinjdt.lib.plugin.event
-
-import fr.valentinjdt.lib.plugin.IPlugin
-
-data class PluginAlreadyLoadedEvent(val plugin: IPlugin): PluginCanNotLoadEvent(plugin.url.path)
